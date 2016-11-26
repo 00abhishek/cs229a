3cee27b47d703ebe651dc40cc7cf9677c80c92cf
rawgni <ingwar.wirjawan@gmail.com>
2016-11-26 08:55:34 +0700

    part 2

--- machine-learning-ex1/ex1/computeCost.m
+++ machine-learning-ex1/ex1/computeCost.m
@@ -13,8 +13,9 @@ J = 0;
 % Instructions: Compute the cost of a particular choice of theta
 %               You should set J to the cost.
 
-
-
+h = X*theta;
+err = (h - y).^2;
+J = sum(err)/(2*m);
 
 
 % =========================================================================
